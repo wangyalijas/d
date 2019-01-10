930bc121b72a27790f65d11e29a811b5525c8399
goodhome <565149480@qq.com>
2019-01-10 18:50:19 +0800

    docs: add highlight.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,6 +2,7 @@
  * 扩展 VuePress 应用
  */
 import Vue from 'vue'
+import HighLight from 'highlight.js'
 import WinDesign from '../../packages/local'
 
 
@@ -9,4 +10,5 @@ export default ({
   Vue
 }) => {
   Vue.use(WinDesign)
+  Vue.prototype.$highLight = HighLight
 }
