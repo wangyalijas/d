7ced946586816fa5a214bf6c49af6542ff504b2d
goodhome <565149480@qq.com>
2019-01-18 14:42:29 +0800

    refactor: add size and close

--- packages/components/tag/src/tag.vue
+++ packages/components/tag/src/tag.vue
@@ -1,10 +1,9 @@
 <template>
-  <transition name="w-zoom-in-center">
-    <div class="w-tag" :class="`w-tag__${type}`">
-      <span class="w-tag__desc"><slot></slot></span>
-      <i v-if="closable" class="w-icon-close2 w-tag__close" @click="hiddenTag"></i>
-    </div>
-  </transition>
+  <span :class="['w-tag', `w-tag--${type}`, `w-tag--${size}`]">
+    <slot></slot>
+    <i v-if="closable" :class="['icon icon-close', `w-tag--close__${type}`]"
+       @click="hiddenTag"></i>
+  </span>
 </template>
 
 <script>
@@ -18,6 +17,10 @@
       type: {
         type: String,
         default: 'primary'
+      },
+      size: {
+        type: String,
+        default: 'normal'
       }
     },
     data () {
@@ -33,38 +36,3 @@
     }
   }
 </script>
-<style lang="scss">
-.w-tag {
-    display: inline-block;
-    height: 32px;
-    line-height: 30px;
-    padding: 0 10px;
-    white-space: nowrap;
-    font-size: 12px;
-    color: #409eff;
-    border-radius: 4px;
-    box-sizing: border-box;
-    border: 1px solid rgba(64,158,255,.2);
-    background-color: rgba(64,158,255,.1);
-}
-.w-tag__success {
-  background-color: rgba(103,194,58,.1);
-  border-color: rgba(103,194,58,.2);
-  color: #67c23a;
-}
-.w-tag__info {
-  background-color: hsla(220,4%,58%,.1);
-  border-color: hsla(220,4%,58%,.2);
-  color: #909399;
-}
-.w-tag__warning {
-  background-color: rgba(230,162,60,.1);
-  border-color: rgba(230,162,60,.2);
-  color: #e6a23c;
-}
-.w-tag__error {
-  background-color: hsla(0,87%,69%,.1);
-  border-color: hsla(0,87%,69%,.2);
-  color: #f56c6c;
-}
-</style>
