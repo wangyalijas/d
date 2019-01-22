ff715c12f30c906dac2f8c30384e0c77d3e8aaa0
goodhome <565149480@qq.com>
2019-01-22 10:05:02 +0800

    feat: add alert component

--- packages/components.js
+++ packages/components.js
@@ -2,10 +2,12 @@ import WTag from './components/tag'
 import WSwitch from './components/switch'
 import WBreadcrumb from './components/breadcrumb'
 import WBreadcrumbItem from './components/breadcrumb-item'
+import WAlert from './components/alert'
 
 export default [
   WTag,
   WSwitch,
   WBreadcrumb,
-  WBreadcrumbItem
+  WBreadcrumbItem,
+  WAlert
 ]
---
+++ packages/components/alert/src/alert.vue
@@ -0,0 +1,73 @@
+<template>
+    <div v-show="visible" :class="['w-alert', typeClass, alignCenter ? 'is-center' : '']" role="alert">
+        <!-- icon -->
+        <i v-if="showIcon" :class="['w-alert__icon', iconClass]"></i>
+        <!-- content -->
+        <div class="w-alert__content">
+            <span v-if="title || $slots.title" :class="['w-alert__title', isBoldTitle]">
+                <slot name="title">{{title}}</slot>
+            </span>
+            <slot>
+                <p v-if="description" v-text="description" class="w-alert__description"></p>
+            </slot>
+        </div>
+        <!-- close icon -->
+        <i v-show="closable" v-text="closeText" :class="{'is-default': closeText !== '', 'w-icon-close': closeText === ''}"
+           class="w-alert__closebutton" @click="handleClose()"></i>
+    </div>
+</template>
+<script>
+export default {
+    name: 'WAlert',
+    data () {
+        return {
+            visible: true
+        }
+    },
+    props: {
+        showIcon: Boolean,
+        alignCenter: Boolean,
+        type: {
+            type: String,
+            default: 'info'
+        },
+        title: {
+            type: String,
+            default: ''
+        },
+        description: {
+            type: String,
+            default: ''
+        },
+        closable: {
+            type: Boolean,
+            default: true
+        },
+        boldTitle: {
+            type: Boolean,
+            default: false
+        },
+        closeText: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        typeClass () {
+            return `w-alert--${this.type}`
+        },
+        iconClass () {
+            return this.type ? `w-icon-${this.type}` : 'w-icon-info'
+        },
+        isBoldTitle () {
+            return this.boldTitle && this.description ? 'is-bold' : ''
+        }
+    },
+    methods: {
+        handleClose () {
+            this.visible = false
+            this.$emit('close')
+        }
+    }
+}
+</script>
