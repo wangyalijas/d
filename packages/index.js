c7f0f673cae68bbb1902fe22ae9b02384dbc552f
goodhome <565149480@qq.com>
2019-01-23 14:27:49 +0800

    feat: add global method

--- packages/index.js
+++ packages/index.js
@@ -1,11 +1,21 @@
 /**
  * 发布用
  */
+import './themes/fonts/iconfont'
 import components from './components'
+import methods from './methods'
 
 const install = (Vue) => {
   if (install.installed) return
   components.forEach(component => Vue.component(component.name, component))
+  let {Loading, Message, MessageBox, Notification} = methods
+  Vue.prototype.$loading = Loading.service
+  Vue.prototype.$msgbox = MessageBox
+  Vue.prototype.$alert = MessageBox.alert
+  Vue.prototype.$confirm = MessageBox.confirm
+  Vue.prototype.$prompt = MessageBox.prompt
+  Vue.prototype.$notify = Notification
+  Vue.prototype.$message = Message
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
---
+++ packages/methods.js
@@ -0,0 +1,10 @@
+import MessageBox from './components/message-box/index.js'
+import Notification from './components/notification/index.js'
+import Loading from './components/loading/index.js'
+import Message from './components/message/index.js'
+export default {
+  Message,
+  Notification,
+  Loading,
+  MessageBox
+}
