fbf620f4170362653dcc700eebd78881dea6ecad
goodhome <565149480@qq.com>
2019-01-23 14:28:15 +0800

    chore: config button install

---
+++ packages/components/button-group/index.js
@@ -0,0 +1,7 @@
+import WButtonGroup from '../button/src/button-group'
+
+WButtonGroup.install = (Vue) => {
+  Vue.component(WButtonGroup.name, WButtonGroup)
+}
+
+export default WButtonGroup
---
+++ packages/components/button/index.js
@@ -0,0 +1,7 @@
+import WButton from './src/button'
+
+WButton.install = (Vue) => {
+  Vue.component(WButton.name, WButton)
+}
+
+export default WButton
