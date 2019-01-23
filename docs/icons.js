edd648b5c4a6007a4e21b0d7843bb4d3f5abba93
goodhome <565149480@qq.com>
2019-01-23 14:21:50 +0800

    chore: add icon list

---
+++ docs/icons.js
@@ -0,0 +1,52 @@
+const common = [{
+  label: 'arrow-left'
+}, {
+  label: 'arrow-right'
+}, {
+  label: 'arrow-down'
+}, {
+  label: 'arrow-up'
+}, {
+  label: 'remove-outline'
+}, {
+  label: 'circle-plus-outline'
+}, {
+  label: 'close'
+}, {
+  label: 'check'
+}, {
+  label: 'view'
+}, {
+  label: 'search'
+}, {
+  label: 'edit'
+}, {
+  label: 'refresh'
+}]
+const bussiness = [{
+  label: 'toulu'
+}, {
+  label: 'erduo'
+}, {
+  label: 'xigainv'
+}, {
+  label: 'dabi'
+}, {
+  label: 'yachi'
+}, {
+  label: 'fei'
+}, {
+  label: 'ganzang'
+}, {
+  label: 'weibu'
+}, {
+  label: 'xigaigu'
+}, {
+  label: 'xizang'
+}, {
+  label: 'zhengrong'
+}, {
+  label: 'zigong'
+}]
+exports.common = common
+exports.bussiness = bussiness
