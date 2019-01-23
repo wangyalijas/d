741b052c70450fe75309fcf679c6db29a787a60b
goodhome <565149480@qq.com>
2019-01-23 15:57:29 +0800

    feat: add icon

---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg class="w-iconx" aria-hidden="true">
+    <use :xlink:href="`#${name}`"></use>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: 'WIconx',
+    props: {
+      name: String
+    }
+  };
+</script>
