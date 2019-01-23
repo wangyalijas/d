d62efc7ef6230347389adbe8c7a5ab2e5dc0f809
goodhome <565149480@qq.com>
2019-01-23 15:37:38 +0800

    fix: import error

--- packages/components/tooltip/src/main.js
+++ packages/components/tooltip/src/main.js
@@ -1,9 +1,9 @@
-import Popper from './../../../utils/vue-popper';
-import debounce from 'throttle-debounce/debounce';
-import { addClass, removeClass, on, off } from './../../../utils/dom';
-import { getFirstComponentChild } from './../../../utils/vdom';
-import { generateId } from './../../../utils/util';
-import Vue from 'vue';
+import Popper from './../../../utils/vue-popper'
+import { debounce } from 'throttle-debounce'
+import { addClass, removeClass, on, off } from './../../../utils/dom'
+import { getFirstComponentChild } from './../../../utils/vdom'
+import { generateId } from './../../../utils/util'
+import Vue from 'vue'
 
 export default {
   name: 'WTooltip',
@@ -32,14 +32,14 @@ export default {
     },
     transition: {
       type: String,
-      default: 'el-fade-in-linear'
+      default: 'w-fade-in-linear'
     },
     popperOptions: {
-      default() {
+      default () {
         return {
           boundariesPadding: 10,
           gpuAcceleration: false
-        };
+        }
       }
     },
     enterable: {
@@ -52,165 +52,165 @@ export default {
     }
   },
 
-  data() {
+  data () {
     return {
       timeoutPending: null,
       focusing: false
-    };
-  },
-  computed: {
-    tooltipId() {
-      return `el-tooltip-${generateId()}`;
     }
   },
-  beforeCreate() {
-    if (this.$isServer) return;
+  computed: {
+    tooltipId () {
+      return `w-tooltip-${generateId()}`
+    }
+  },
+  beforeCreate () {
+    if (this.$isServer) return
 
     this.popperVM = new Vue({
       data: { node: '' },
-      render(h) {
-        return this.node;
+      render (h) {
+        return this.node
       }
-    }).$mount();
+    }).$mount()
 
-    this.debounceClose = debounce(200, () => this.handleClosePopper());
+    this.debounceClose = debounce(200, () => this.handleClosePopper())
   },
 
-  render(h) {
+  render (h) {
     if (this.popperVM) {
       this.popperVM.node = (
         <transition
           name={ this.transition }
           onAfterLeave={ this.doDestroy }>
           <div
-            onMouseleave={ () => { this.setExpectedState(false); this.debounceClose(); } }
-            onMouseenter= { () => { this.setExpectedState(true); } }
+            onMouseleave={ () => { this.setExpectedState(false); this.debounceClose() } }
+            onMouseenter= { () => { this.setExpectedState(true) } }
             ref="popper"
             role="tooltip"
             id={this.tooltipId}
             aria-hidden={ (this.disabled || !this.showPopper) ? 'true' : 'false' }
             v-show={!this.disabled && this.showPopper}
             class={
-              ['el-tooltip__popper', 'is-' + this.effect, this.popperClass]
+              ['w-tooltip__popper', 'is-' + this.effect, this.popperClass]
             }>
             { this.$slots.content || this.content }
           </div>
-        </transition>);
+        </transition>)
     }
 
-    if (!this.$slots.default || !this.$slots.default.length) return this.$slots.default;
+    if (!this.$slots.default || !this.$slots.default.length) return this.$slots.default
 
-    const vnode = getFirstComponentChild(this.$slots.default);
+    const vnode = getFirstComponentChild(this.$slots.default)
 
-    if (!vnode) return vnode;
+    if (!vnode) return vnode
 
-    const data = vnode.data = vnode.data || {};
-    data.staticClass = this.concatClass(data.staticClass, 'el-tooltip');
+    const data = vnode.data = vnode.data || {}
+    data.staticClass = this.concatClass(data.staticClass, 'w-tooltip')
 
-    return vnode;
+    return vnode
   },
 
-  mounted() {
-    this.referenceElm = this.$el;
+  mounted () {
+    this.referenceElm = this.$el
     if (this.$el.nodeType === 1) {
-      this.$el.setAttribute('aria-describedby', this.tooltipId);
-      this.$el.setAttribute('tabindex', 0);
-      on(this.referenceElm, 'mouseenter', this.show);
-      on(this.referenceElm, 'mouseleave', this.hide);
+      this.$el.setAttribute('aria-describedby', this.tooltipId)
+      this.$el.setAttribute('tabindex', 0)
+      on(this.referenceElm, 'mouseenter', this.show)
+      on(this.referenceElm, 'mouseleave', this.hide)
       on(this.referenceElm, 'focus', () => {
         if (!this.$slots.default || !this.$slots.default.length) {
-          this.handleFocus();
-          return;
+          this.handleFocus()
+          return
         }
-        const instance = this.$slots.default[0].componentInstance;
+        const instance = this.$slots.default[0].componentInstance
         if (instance && instance.focus) {
-          instance.focus();
+          instance.focus()
         } else {
-          this.handleFocus();
+          this.handleFocus()
         }
-      });
-      on(this.referenceElm, 'blur', this.handleBlur);
-      on(this.referenceElm, 'click', this.removeFocusing);
+      })
+      on(this.referenceElm, 'blur', this.handleBlur)
+      on(this.referenceElm, 'click', this.removeFocusing)
     }
   },
   watch: {
-    focusing(val) {
+    focusing (val) {
       if (val) {
-        addClass(this.referenceElm, 'focusing');
+        addClass(this.referenceElm, 'focusing')
       } else {
-        removeClass(this.referenceElm, 'focusing');
+        removeClass(this.referenceElm, 'focusing')
       }
     }
   },
   methods: {
-    show() {
-      this.setExpectedState(true);
-      this.handleShowPopper();
+    show () {
+      this.setExpectedState(true)
+      this.handleShowPopper()
     },
 
-    hide() {
-      this.setExpectedState(false);
-      this.debounceClose();
+    hide () {
+      this.setExpectedState(false)
+      this.debounceClose()
     },
-    handleFocus() {
-      this.focusing = true;
-      this.show();
+    handleFocus () {
+      this.focusing = true
+      this.show()
     },
-    handleBlur() {
-      this.focusing = false;
-      this.hide();
+    handleBlur () {
+      this.focusing = false
+      this.hide()
     },
-    removeFocusing() {
-      this.focusing = false;
-    },
-
-    concatClass(a, b) {
-      if (a && a.indexOf(b) > -1) return a;
-      return a ? b ? (a + ' ' + b) : a : (b || '');
+    removeFocusing () {
+      this.focusing = false
     },
 
-    handleShowPopper() {
-      if (!this.expectedState || this.manual) return;
-      clearTimeout(this.timeout);
+    concatClass (a, b) {
+      if (a && a.indexOf(b) > -1) return a
+      return a ? b ? (a + ' ' + b) : a : (b || '')
+    },
+
+    handleShowPopper () {
+      if (!this.expectedState || this.manual) return
+      clearTimeout(this.timeout)
       this.timeout = setTimeout(() => {
-        this.showPopper = true;
-      }, this.openDelay);
+        this.showPopper = true
+      }, this.openDelay)
 
       if (this.hideAfter > 0) {
         this.timeoutPending = setTimeout(() => {
-          this.showPopper = false;
-        }, this.hideAfter);
+          this.showPopper = false
+        }, this.hideAfter)
       }
     },
 
-    handleClosePopper() {
-      if (this.enterable && this.expectedState || this.manual) return;
-      clearTimeout(this.timeout);
+    handleClosePopper () {
+      if ((this.enterable && this.expectedState) || this.manual) return
+      clearTimeout(this.timeout)
 
       if (this.timeoutPending) {
-        clearTimeout(this.timeoutPending);
+        clearTimeout(this.timeoutPending)
       }
-      this.showPopper = false;
+      this.showPopper = false
 
       if (this.disabled) {
-        this.doDestroy();
+        this.doDestroy()
       }
     },
 
-    setExpectedState(expectedState) {
+    setExpectedState (expectedState) {
       if (expectedState === false) {
-        clearTimeout(this.timeoutPending);
+        clearTimeout(this.timeoutPending)
       }
-      this.expectedState = expectedState;
+      this.expectedState = expectedState
     }
   },
 
-  destroyed() {
-    const reference = this.referenceElm;
-    off(reference, 'mouseenter', this.show);
-    off(reference, 'mouseleave', this.hide);
-    off(reference, 'focus', this.handleFocus);
-    off(reference, 'blur', this.handleBlur);
-    off(reference, 'click', this.removeFocusing);
+  destroyed () {
+    const reference = this.referenceElm
+    off(reference, 'mouseenter', this.show)
+    off(reference, 'mouseleave', this.hide)
+    off(reference, 'focus', this.handleFocus)
+    off(reference, 'blur', this.handleBlur)
+    off(reference, 'click', this.removeFocusing)
   }
-};
+}
